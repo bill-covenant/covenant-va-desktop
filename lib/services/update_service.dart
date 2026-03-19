eef5e804447b4a94d3352970136c28d49dc3380a
JB Garcia <billg@covenant-va.com>
2026-03-19 14:53:23 +0800

    1.0.5 update

--- lib/services/update_service.dart
+++ lib/services/update_service.dart
@@ -22,8 +22,8 @@ class UpdateInfo {
 
 class UpdateService {
   // ⚠️ Update this every time you release a new version
-  static const String currentVersion = '1.0.4';
-  static const int currentBuildNumber = 5;
+  static const String currentVersion = '1.0.5';
+  static const int currentBuildNumber = 6;
 
   static Future<UpdateInfo?> checkForUpdate(String apiBaseUrl) async {
     try {
